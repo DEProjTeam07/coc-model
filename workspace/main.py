8263e152e97fb0816eca40a5354a866dfd01ddef
Ubuntu <ubuntu@ip-172-31-15-63.ap-northeast-2.compute.internal>
2024-09-10 08:21:20 +0000

    feat : 모델 수정

--- workspace/main.py
+++ workspace/main.py
@@ -1,7 +1,13 @@
 import torch
 from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+
 
 from src.S3ImageDatasets import S3ImageDatasets
+#from src.models.Efficientnets import Efficientnets
+from src.models.Resnet import ResNet18
+from src.Train import train_model
 
 AWS_BUCKET_NAME = 'deprojteam07-labeledrawdata'
 
@@ -9,10 +15,25 @@ dataset = S3ImageDatasets(bucket_name=AWS_BUCKET_NAME,usage='train')
 
 train_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 
-print(train_loader)
+dataset = S3ImageDatasets(bucket_name=AWS_BUCKET_NAME,usage='test')
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# model = model.to(device)
+test_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 
-# train_model(model, criterion, optimizer, train_loader, test_loader, num_epochs=20)
 
+
+#model = Efficientnets()
+model = ResNet18()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)  # weight decay 추가
+
+train_losses = []
+test_losses = []
+train_accuracies = []
+test_accuracies = []
+
+trained_model = train_model(model=model, device=device, criterion=criterion, optimizer=optimizer, train_loader=train_loader,
+            test_loader=test_loader)
--- workspace/src/Train.py
+++ workspace/src/Train.py
@@ -1,58 +1,75 @@
-# 5. 손실 함수 및 옵티마이저 설정 (weight decay 적용)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)  # weight decay 추가
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import numpy as np
 
-# 6. 손실 및 정확도 추적을 위한 리스트
 train_losses = []
 test_losses = []
 train_accuracies = []
 test_accuracies = []
 
-# 7. 학습 및 평가 함수
-def train_model(model, criterion, optimizer, train_loader, test_loader, num_epochs=10):
+def train_model(model,device, criterion, optimizer, train_loader, test_loader, num_epochs=10,save_file='model_state_dict.pth'):
     for epoch in range(num_epochs):
+        #최소 손실값 초기화
+        valid_loss_min = np.inf
+
+        #train
         model.train()
-        running_loss = 0.0
+        running_loss = 0.0 # 손실값 초기화
         correct = 0
         total = 0
 
-        # 8. 학습 단계
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            optimizer.zero_grad() #옵티마이저 내 기울기 초기화
+            outputs = model(inputs) #순전파
+            loss = criterion(outputs, labels) #손실값 계산
+            loss.backward() #역전파
+            optimizer.step() #가중치 갱신
 
-            running_loss += loss.item()
+            running_loss += loss.item() #현재 배치에서의 손실 추가
+
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
+        
         train_loss = running_loss / len(train_loader)
         train_acc = 100 * correct / total
         train_losses.append(train_loss)
         train_accuracies.append(train_acc)
 
-        # 9. 평가 단계
+        print(f'/t훈련데이터 손실값: {train_loss:.4f}/t훈련데이터 정확도: {train_acc}/t epoch({epoch}/{num_epochs})')
+
+        #validation
         model.eval()
         test_loss = 0.0
         correct = 0
         total = 0
-        with torch.no_grad():
+
+        with torch.no_grad(): #기울기 계산 비활성화
             for inputs, labels in test_loader:
                 inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                test_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
+                outputs = model(inputs) #순전파
+                loss = criterion(outputs, labels) #손실값 계산
+                test_loss += loss.item() #현재 배치에서의 손실 추가
+
+                _, predicted = torch.max(outputs, 1) #예측값 및 실제값
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-
+        
         test_loss = test_loss / len(test_loader)
         test_acc = 100 * correct / total
         test_losses.append(test_loss)
         test_accuracies.append(test_acc)
 
         print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%, Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}%')
+
+        #최적 모델 가중치 찾기
+        ## 현재 에포크에서 손실값이 최소 손실값 이하이면 모델 가중치 저장
+        if train_loss <= valid_loss_min:
+            print(f'\t@@@ 검증 데이터 손실값 감소 ({valid_loss_min:.4f} --> {train_loss:.4f})\n\t모델저장')
+
+            #모델 저장
+            torch.save(model.state_dict(), save_file)
+            valid_loss_min = train_loss
+    return torch.load(save_file)
