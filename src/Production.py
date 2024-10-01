bfa7822c8a7102addad912ad118f7394c534653f
duri-wip <8s.eow.ooc@gmail.com>
2024-10-01 07:23:12 +0000

    S3 dataset 추가시 Github Action Workflow 동작

--- src/Production.py
+++ src/Production.py
@@ -7,8 +7,10 @@ def production_model_info():
     client = mlflow.tracking.MlflowClient()
     version = client.search_model_versions("name='Production'")
 
-    run_id = version[0].run_id
-    model_name = version[0].tags.get('model_name')
-    model_uri=f"runs:/{run_id}/{model_name}"
-    
+    if len(version) > 1:
+        print("운영 모델이 2개 이상입니다.")
+    else:
+        run_id = version[0].run_id
+        model_name = version[0].tags.get('model_name')
+        model_uri=f"runs:/{run_id}/{model_name}"
     return model_uri
