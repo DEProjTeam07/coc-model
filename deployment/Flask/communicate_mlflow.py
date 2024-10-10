4d13ba301c170db333eb529892c3ed377df97089
Ubuntu <ubuntu@ip-172-31-0-17.ap-northeast-2.compute.internal>
2024-10-10 02:00:28 +0000

    communicate_mlflow 맨 아래 전체 주석 제거

--- deployment/Flask/communicate_mlflow.py
+++ deployment/Flask/communicate_mlflow.py
@@ -56,33 +56,3 @@ def get_production_model_uri():
     
     return model_uri
 
-
-
-
-
-
-
-
-
-# mlflow Model Registry에 Production Name에 모델의 run id를 가져오는 함수 
-# def get_production_run_id():
-#     client = mlflow.tracking.MlflowClient()
-#     version = client.search_model_versions("name='Production'")
-
-#     run_id = version[0].run_id
-    
-#     return run_id 
-
-    
-# mlflow Model Registry에 Production Name에 모델의 model_uri를 가져오는 함수 
-# def get_production_model_uri():
-#     client = mlflow.tracking.MlflowClient()
-#     version = client.search_model_versions("name='Production'")
-
-#     if len(version) > 1:
-#         print("운영 모델이 2개 이상입니다.")
-#     else:
-#         run_id = version[0].run_id
-#         model_name = version[0].tags.get('model_name')
-#         model_uri=f"runs:/{run_id}/{model_name}"
-#     return model_uri
